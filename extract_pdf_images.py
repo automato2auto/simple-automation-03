d99e96216b7b4719674c16e799ac2261c037318c
tarek-harba <tarek.harba01@gmail.com>
2023-08-04 16:01:26 +0300

    code commit

---
+++ extract_pdf_images.py
@@ -0,0 +1,28 @@
+# استيراد للأدوات والمكاتب التي ستساعدنا
+import fitz
+from PIL import Image
+
+# فتح ملف ال PDF
+doc = fitz.open('مسار_الملف.pdf')
+
+# القائمة يلي رح نحفظ فيها الصور المستخرجة
+images_list = []
+
+# استخراج  معلومات الصور من صفحات الملف وإضافتها لقائمة الصور
+for page in doc:
+    images_list.extend(page.get_images())
+
+# في حال لم يكن هنالك صور يتم طباعة رسالة بذلك
+if not images_list:
+    print(f'{file_path} has no images')
+
+# حفظ الصور إلى الجهاز في مجلد img
+for i, img in enumerate(images_list, start=1):
+    # استخراج بيانات الصورة من الملف
+    base_image = doc.extract_image(img[0])
+    image_name = str(i) + '.' + base_image['ext']
+    # حفظ الصورة إلى الجهاز
+    with open(f'./img/{image_name}', 'wb') as image_file:
+        image_file.write(base_image['image'])
+        image_file.close()
+# <a href="https://www.freepik.com/icons/pic">Icon by Freepik</a>
